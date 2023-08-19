46681451b1bf89d4bf32dd2499fa144c4fb07e46
chiji1108 <shingo.pad@gmail.com>
2023-08-19 13:58:49 +0900

    supabase init

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -5,14 +5,14 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import type { Database } from "@/lib/database.types";
 
+export const dynamic = "force-dynamic";
+
 export async function GET(request: NextRequest) {
   const requestUrl = new URL(request.url);
   const code = requestUrl.searchParams.get("code");
 
   if (code) {
-    const supabase = createRouteHandlerClient<Database>({
-      cookies,
-    });
+    const supabase = createRouteHandlerClient<Database>({ cookies });
     await supabase.auth.exchangeCodeForSession(code);
   }
 
